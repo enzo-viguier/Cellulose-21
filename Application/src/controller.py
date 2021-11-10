0354b252ab172609a24ad4d31ab6ae5636b9cefb
fressardm <fressardmelanie@gmail.com>
2021-11-10 11:11:06 +0100

    debut de notre controller

--- Application/src/controller.py
+++ Application/src/controller.py
@@ -3,30 +3,42 @@ import simulation
 from PyQt5.QtWidgets import QMainWindow
 
 
-# existant :
-def showAll():
-    pass
+# # essais avec les tuto :
 
-def start():
-    simulation.startView()
-    input = raw_input()
-    if input == 'y':
-        return showAll()
-    else:
-        return simulation.endView()
+# def start():
+#     simulation.startView()
 
-if __name__ == "__main__":
-    #running controller function
-    start()
+#    # if :
+#         #return simulation.endView()
 
-#ce que joannides a fait :
+# if __name__ == "__init__":
+#     #running controller function
+#     start()
+
+# #ce que joannides a fait :
+# class control(QMainWindow):
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#         self.ui = Ui_Cellulose()
+#         self.ui.setupUi(self)
+# #      self.ui.updateButton.clicked.connect(self.update_view)
+#         self.m = Modele(self.ui.mplView)
+# #        self.m.stateChangedSignal.connect(self.update_view)
+
+        
+        
+#     def update_view(self):
+#         self.ui.mplView.data_ref.set_data(self.m.getData())
+#         self.ui.mplView.draw()
+#         pass
+
 class control(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.ui = Ui_Cellulose()
+        self.ui = Ui_Simulation()
         self.ui.setupUi(self)
 #      self.ui.updateButton.clicked.connect(self.update_view)
-        self.m = Model(self.ui.mplView)
+        self.m = Modele(self.ui.mplView)
 #        self.m.stateChangedSignal.connect(self.update_view)
 
         
