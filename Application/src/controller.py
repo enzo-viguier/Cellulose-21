1e5e146f412be2b98d38e433f6296366e1c2d8e2
fressardm <fressardmelanie@gmail.com>
2021-11-10 10:41:15 +0100

    modif du controller

--- Application/src/controller.py
+++ Application/src/controller.py
@@ -1,20 +1,37 @@
-from modele import Model
+from modele import Modele
 import simulation
+from PyQt5.QtWidgets import QMainWindow
 
+
+# existant :
 def showAll():
-    #gets list of all Person objects
-    people_in_db = Person.getAll()
-    #calls view
-    return view.showAllView(people_in_db)
+    pass
 
 def start():
-    view.startView()
+    simulation.startView()
     input = raw_input()
     if input == 'y':
         return showAll()
     else:
-        return view.endView()
+        return simulation.endView()
 
 if __name__ == "__main__":
     #running controller function
     start()
+
+#ce que joannides a fait :
+class control(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Cellulose()
+        self.ui.setupUi(self)
+#      self.ui.updateButton.clicked.connect(self.update_view)
+        self.m = Model(self.ui.mplView)
+#        self.m.stateChangedSignal.connect(self.update_view)
+
+        
+        
+    def update_view(self):
+        self.ui.mplView.data_ref.set_data(self.m.getData())
+        self.ui.mplView.draw()
+        pass
