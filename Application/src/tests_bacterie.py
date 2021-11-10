fd1200733e651a788fb0df1aa76c5eb1b634a1d4
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2021-11-10 17:38:12 +0100

    modif erreurs TestBacterie

--- Application/src/tests_bacterie.py
+++ Application/src/tests_bacterie.py
@@ -1,17 +1,19 @@
 from model import *
 from bacterie import *
 
+
 class TestBacterie:
     def test_calcul_vitesse_deplacement(self):
-            model = Model()
-            model.init_d_tore(1, 10, 10)
-            model.init_d_cellulose(c_min=0, c_ini=2)
-            model.creer_concentrations(10, 1)
-            
-            bact = Bacterie(model, 0, 0, 1, v_absorb=0.3)
-            print("coordonnées xy bactérie avant : ", bact.get_coord_xy())
-            vd_x, vd_y = bact.calcul_vitesse_deplacement()
-            print("coordonnées xy bactérie après : ", vd_x, vd_y)
+        model = Model()
+        model.init_d_tore(1, 10, 10)
+        model.init_d_cellulose(c_min=0, c_ini=2)
+        model.creer_concentrations(10, 1)
+
+        bact = Bacterie(model, 0, 0, 1, v_absorb=0.3)
+        print("coordonnées xy bactérie avant : ", bact.get_coord_xy())
+        vd_x, vd_y = bact.__calcul_vitesse_deplacement()
+        print("coordonnées xy bactérie après : ", vd_x, vd_y)
+
 
 test = TestBacterie()
 test.test_calcul_vitesse_deplacement()
