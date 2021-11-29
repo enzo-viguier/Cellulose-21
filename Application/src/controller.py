3601c75cb6e357cba8db2509b8913bfb6ec98029
fressardm <fressardmelanie@gmail.com>
2021-11-29 17:29:54 +0100

    ajout des 2 autres instances mpl

--- Application/src/controller.py
+++ Application/src/controller.py
@@ -4,36 +4,6 @@ from model import Model
 import Ui_Simulation
 from PyQt5.QtWidgets import QMainWindow
 
-
-# # essais avec les tuto :
-
-# def start():
-#     simulation.startView()
-
-#    # if :
-#         #return simulation.endView()
-
-# if __name__ == "__init__":
-#     #running controller function
-#     start()
-
-# #ce que joannides a fait :
-# class control(QMainWindow):
-#     def __init__(self, parent=None):
-#         super().__init__(parent)
-#         self.ui = Ui_Cellulose()
-#         self.ui.setupUi(self)
-# #      self.ui.updateButton.clicked.connect(self.update_view)
-#         self.m = Modele(self.ui.mplView)
-# #        self.m.stateChangedSignal.connect(self.update_view)
-
-        
-        
-#     def update_view(self):
-#         self.ui.mplView.data_ref.set_data(self.m.getData())
-#         self.ui.mplView.draw()
-#         pass
-
 class control(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -47,4 +17,7 @@ class control(QMainWindow):
     def update_view(self):
         self.ui.animationSubstrat.data_ref.set_data(self.m.concentrations)
         self.ui.animationSubstrat.draw()
-        pass
+        self.ui.graph_1.data_ref.set_data(self.m.concentrations)
+        self.ui.graph_1.draw()
+        self.ui.graph_2.data_ref.set_data(self.m.concentrations)
+        self.ui.graph_2.draw()
