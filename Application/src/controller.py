547c4d57c623da381237ded8489744c1861fad26
Gatien-Dep <gatien.depeyre@etu.umontpellier.fr>
2021-12-08 12:40:12 +0100

    controller netoyé

--- Application/src/controller.py
+++ Application/src/controller.py
@@ -10,13 +10,10 @@ class control(QMainWindow):
         self.ui = Ui_Simulation.Ui_MainWindow()
         self.ui.setupUi(self)
         #self.ui.updateButton.clicked.connect(self.update_view)
-        self.m = Model(self.ui)
+        self.m = Model(self.ui, c_ini=0)
         self.m.stateChangedSignal.connect(self.update_view)
-        self.start()
-
-    def start(self):
         self.m.start()
-    
+
     def update_view(self):
         self.ui.animationSubstrat.data_ref.set_data(self.m.concentrations)
         self.ui.animationSubstrat.draw()
