f29500b21ff3660b58b660f2656b02d8910be5cd
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2021-12-27 15:44:24 +0100

    Suppression de commentaires

--- Application/src/bacterie.py
+++ Application/src/bacterie.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Bacterie:
     # Mettre ici les variables statiques
     # Dictionnaire de constantes de la biomasse
@@ -28,17 +29,16 @@ class Bacterie:
         if self.model.get_concentration_by_coord_xy((self.x, self.y)) <= self.model.d_cellulose["c_min"]:
 
             delta = self.model.d_tore["delta"]
-            demi_longueur = self.model.d_tore["longueur"]/2
+            demi_longueur = self.model.d_tore["longueur"] / 2
             vd_x, vd_y = self.__calcul_vitesse_deplacement()
-            self.x = self.x + delta*vd_x + self.model.d_biomasse["b_diff"] * np.sqrt(delta) * self.random()
-            # np.random.rand() ∈ [0;1]
+            self.x = self.x + delta * vd_x + self.model.d_biomasse["b_diff"] * np.sqrt(delta) * self.random()
             # Si les coordonnées sortent de l'environnement, les faire passer de l'autre côté du tore
             if self.x > demi_longueur:
                 self.x = -demi_longueur
             elif self.x < -demi_longueur:
                 self.x = demi_longueur
 
-            self.y = self.y + delta*vd_y + self.model.d_biomasse["b_diff"] * np.sqrt(delta) * self.random()
+            self.y = self.y + delta * vd_y + self.model.d_biomasse["b_diff"] * np.sqrt(delta) * self.random()
             # Si les coordonnées sortent de l'environnement, les faire passer de l'autre côté du tore
             if self.y > demi_longueur:
                 self.y = -demi_longueur
@@ -46,10 +46,12 @@ class Bacterie:
                 self.y = demi_longueur
 
     def random(self):
-        r = (np.random.rand()-0.5)*2
+        """
+        :return: float ∈ [-1;1]
+        """
+        r = (np.random.rand() - 0.5) * 2  # np.random.rand() ∈ [0;1]
         print("random = ", r)
         return r
-
 
     def __calcul_vitesse_deplacement(self):
         """
@@ -59,18 +61,10 @@ class Bacterie:
         :return: tuple
         """
         vd = self.model.d_biomasse["vd"]
-        # Convertir coordonnées x,y en i,j
-        # (i, j) = self.model.convert_coord_xy_to_ij((self.x, self.y))
-        # print("My i = ", i, ". My j = ", j)
-        # Exemple des lignes suivantes décomposées
-        # c_est = self.model.get_concentration_by_coord_ij((i + 1, j))
-        # c_ouest = self.model.get_concentration_by_coord_ij((i - 1, j))
-        # c_nord = self.model.get_concentration_by_coord_ij((i, j + 1))
-        # c_sud = self.model.get_concentration_by_coord_ij((i, j - 1))
-        c_est = self.model.get_concentration_by_coord_xy((self.x+1, self.y))
-        c_ouest = self.model.get_concentration_by_coord_xy((self.x-1, self.y))
-        c_nord = self.model.get_concentration_by_coord_xy((self.x, self.y+1))
-        c_sud = self.model.get_concentration_by_coord_xy((self.x, self.y-1))
+        c_est = self.model.get_concentration_by_coord_xy((self.x + 1, self.y))
+        c_ouest = self.model.get_concentration_by_coord_xy((self.x - 1, self.y))
+        c_nord = self.model.get_concentration_by_coord_xy((self.x, self.y + 1))
+        c_sud = self.model.get_concentration_by_coord_xy((self.x, self.y - 1))
         h = self.model.d_tore["largeur_case"]
         print("c_est", c_est)
         print("c_ouest", c_ouest)
@@ -95,17 +89,17 @@ class Bacterie:
         # main_case = self.modele.get_concentration_by_coord_ij(coords_ij)
         for i in np.arange(-1, 2):
             for j in np.arange(-1, 2):
-
                 # Vérification que les coordonnées ne sortent pas du tableau avec l'incrementation
-                coords_ij = (coords_ij_centre[0]+i, coords_ij_centre[1]+j)
+                coords_ij = (coords_ij_centre[0] + i, coords_ij_centre[1] + j)
 
                 # On prend les cases autour du centre ainsi que le centre
                 case = self.model.get_concentration_by_coord_ij(coords_ij)
-                conso = np.minimum(np.square(self.model.d_tore["largeur_case"]) * case, self.model.d_biomasse["v_absorb"])
+                conso = np.minimum(np.square(self.model.d_tore["largeur_case"]) * case,
+                                   self.model.d_biomasse["v_absorb"])
                 # carre à verifier
                 qt_mange += conso
-                self.model.set_concentration_by_coord_ij(coords_ij, case - (conso / np.square(self.model.d_tore["largeur_case"])))
-
+                self.model.set_concentration_by_coord_ij(coords_ij,
+                                                         case - (conso / np.square(self.model.d_tore["largeur_case"])))
 
     def gain_masse(self, conso):
         """
@@ -119,6 +113,6 @@ class Bacterie:
         Objectif : Déterminer si la bactérie peut se dupliquer
         :return: boolean
         """
-        if self.masse_act > self.model.d_biomasse["masse_ini"]*2:
+        if self.masse_act > self.model.d_biomasse["masse_ini"] * 2:
             return True
         return False
